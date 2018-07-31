81f3c049d2cc7961508fd86e5aca5d96c1e200e6
Cam K Weston <cameron.weston@uvm.edu>
2018-07-30 21:03:53 -0400

    First commit with about me nav and header

---
+++ aboutme.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=.5"/>
+  <title>CamWeston.com</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet">
+  <link href="css/materialize.css?<?php echo time(); ?>" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css?<?php echo time(); ?>" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+
+<body>
+  <header class ="header blue z-depth-1 center">
+    <div class='headerTop'>
+    <div class="valign-wrapper">
+    <img class = "circle responsive-img top" src="assets/profilePic.jpeg">
+    <div class="container">
+    <h2>Cam Weston</h1>
+    <h3>Creative. Ambitious. Developer.</h2>
+    </div>
+    </div>
+    </div>
+     </header>
+   <nav>
+    <div class="nav-wrapper">
+    <div class="container">
+      <div class="row">
+      <ul>
+        <li class='col'><a href="aboutMe.html">About Me</a></li>
+        <li class='col'><a href="workexperience.html">Work Experience</a></li>
+        <li class='col'><a href="projects.html">Projects</a></li>
+        <li class='col'><a href="blogs.html">Blogs</a></li>
+      </ul>
+    </div> 
+  </div>
+  </div>
+  </nav>
+
+
+<div class="preloader-background">
+  <div class="preloader-wrapper big active">
+    <div class="spinner-layer spinner-blue-only">
+      <div class="circle-clipper left">
+        <div class="circle"></div>
+      </div>
+      <div class="gap-patch">
+        <div class="circle"></div>
+      </div>
+      <div class="circle-clipper right">
+        <div class="circle"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <div class='container'>
+  <div class='row'>
+  <h4 class="underline left-align">My Skills</h4>
+  <div class ='aboutMe'>
+  <p id = "intro">
+My name is Cam Weston and I am an ambitious and creative software engineer
+who is passionate about technology, leadership and finding ways to change the world. 
+  </p>
+    <div class='row'>
+      <div class ='center-align container col s12 m12 l4'>
+        <div class="priority z-depth-3">
+          <img class ='responsive-img' src='assets/brain.png'>
+          <h5>User Experience</h6>
+          <p>Apps that keep the user entertained and coming back</p>
+       </div>
+      </div>
+      <div class ='center-align container col s12 m12 l4'>
+        <div class="priority z-depth-3">  
+          <img class ='responsive-img' src='assets/paint.png'>
+        <h5>Intuitive</h6>
+        <p>Apps so easy your grandma could use them</p>
+      </div>
+      </div>
+      <div class ='center-align container col s12 m12 l4'>
+        <div class="priority z-depth-3">
+          <img class ='center responsive-img' src='assets/speedometer.png'>
+        <h5>Fast</h5>
+        <p>Apps that won't keep you waiting</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+ 
+  <div class="parallax-container parrallax-medium">
+  <div class="parallax"><img src="assets/burlington.jpg"></div>
+  </div>
+  <div class='container'>
+  <div class='row'>
+  <h4 class="underline left-align">My Skills</h4>
+  <div class='left-container col s12 m12 l12'>
+     <h5>Languages</h5><hr/>
+      <ul class = "collection">         
+         <li class = "collection-item">
+            <div>Java<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/javaicon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>C++<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/cplusplus.png"></a></div></li>
+         <li class = "collection-item"> 
+            <div>C#<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/csharpicon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>Python<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/pythonicon.png"></a></div></li> 
+         <li class = "collection-item">
+            <div>JavaScript<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/jsicon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>PHP<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/phpicon.png"></a></div></li> 
+         <li class = "collection-item">
+            <div>SQL<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/sqlicon.png"></a></div></li> 
+         <li class = "collection-item">
+            <div>HTML 5<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/html5icon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>CSS<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/cssicon.png"></a></div></li>
+      </ul>
+  </div>
+  <div class='middle-container col s12 m12 l12'>
+   <h5>Frameworks</h5><hr/>
+      <ul class = "collection">         
+         <li class = "collection-item">
+            <div>Spring MVC<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/springicon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>Materialize CSS<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/materializeicon.png"></a></div></li>
+         <li class = "collection-item"> 
+            <div>Bootstrap<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/bootstrapicon.png"></a></div></li>
+      </ul>
+  </div>
+  <div class='right-container col s12 m12 l12'> 
+    <h5>Tools</h5><hr/>
+      <ul class = "collection">         
+         <li class = "collection-item">
+            <div>AWS Services<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/awsicon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>JQuery<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/jqueryicon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>Git<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/giticon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>Tortise SVN<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/tortoiseicon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>JIRA<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/jiraicon.png"></a></div></li>
+         <li class = "collection-item">
+            <div>JUnit<a href = "#!" class = "secondary-content">
+               <img class = "list-icon circle" src="assets/icons/juniticon.png"></a></div></li>
+      </ul>
+    </div> 
+  </div>
+  </div>
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
